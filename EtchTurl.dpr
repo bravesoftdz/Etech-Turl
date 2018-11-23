beb5814f93a79538eec98855682b51290c2183de
willcruz07 <lynkan@hotmail.com>
2018-11-23 00:15:50 -0200

    Ajuste no projeto

--- EtchTurl.dpr
+++ EtchTurl.dpr
@@ -2,21 +2,23 @@ program EtchTurl;
 
 uses
   Vcl.Forms,
+  Vcl.Styles,
   uPrincipal in 'view\uPrincipal.pas' {frmPrincipal},
   uLogin in 'view\uLogin.pas' {frmLogin},
-  Vcl.Themes,
-  Vcl.Styles,
-  uEntradaNF in 'view\uEntradaNF.pas' {frmEntradaNF},
-  uBaixaEstoque in 'view\uBaixaEstoque.pas' {frmBaixaEstoque};
+  uFormBase in 'view\uFormBase.pas' {frmBase},
+  uFormEntradaNF in 'view\uFormEntradaNF.pas' {frmEntradaNF},
+  uFormBaixaEstoque in 'view\uFormBaixaEstoque.pas' {frmBaixaEstoque},
+  Classe.Request in 'model\Classe.Request.pas',
+  Classe.DiretorioWEB in 'model\Classe.DiretorioWEB.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  TStyleManager.TrySetStyle('Aqua Light Slate');
   Application.CreateForm(TfrmPrincipal, frmPrincipal);
   Application.CreateForm(TfrmLogin, frmLogin);
+  Application.CreateForm(TfrmBase, frmBase);
   Application.CreateForm(TfrmEntradaNF, frmEntradaNF);
   Application.CreateForm(TfrmBaixaEstoque, frmBaixaEstoque);
   Application.Run;
